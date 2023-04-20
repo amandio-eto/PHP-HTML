52d97222e7fc779f3f16110881aa57e5381daa8e
amandio-eto <JoseAmandiodeAlmeida021099@gmail.com>
2023-04-20 14:41:34 +0900

    update print statment

---
+++ Print_statment.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <h1>
+      
+      <?php
+       /*
+       
+       in PHP
+        there are many print statements to Display the Output for our program code
+        1) Print -> is also the output result in PHP too but this command only displays one data t
+        2) echo -> is an output command for what you really need and really uses in PHP to display more than one data
+        3) var_dump()-> is a code command in PHP to display data in the form of arrays, objects and strings
+        Usually this command is used to debug program or application code
+       
+       */
+       
+       
+       $coutry = "Timor-Leste";
+       
+       print "$coutry";
+       echo "$coutry";
+       var_dump($coutry);
+       
+       
+       
+       
+       
+       ?>
+       
+       
+   </h1>
+    
+</body>
+</html>
