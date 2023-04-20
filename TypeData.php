17e4d3a6182172de8e05ffae260016dc700ea94c
amandio-eto <JoseAmandiodeAlmeida021099@gmail.com>
2023-04-20 17:15:25 +0900

    update Typedata

---
+++ TypeData.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>
+    
+    Type Data 
+    </title>
+</head>
+<body>
+  
+  <h4>Type Data</h4>
+   
+   <?php
+    $name = "Jose Amandio de Almeida";
+    $age = 23;
+    $cash = 100.00;
+    $array = ["Coding",'reading'];
+    
+    
+    
+    echo "String ".$name."<br>";
+    echo "Is Boolean ".true."<br>";
+    echo "Integer ".$age."<br>";
+    echo "floating point ".($cash)."<br>";
+    echo "Is Null ".null."<br>";
+    var_dump($array);
+    
+
+    
+    
+    ?>
+    
+</body>
+</html>
---
+++ TypeData_Array_numeric.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <h4>This is The Array Numeric </h4>
+<?php
+    
+// This is I Used to be how to use Array and Get Data 
+    
+/*
+
+Data in Array have Index start 0 ussaly use [] or in PHP can use array();
+
+example for  $data =[index 0,index 1, index 2];
+
+if you want go get data in array exaple array index 2;
+
+you can call like this 
+
+$data[index 2];
+
+how to Practise Realtime
+    
+    
+*/
+    
+    $data = ['apple','Juice','orange'];
+
+    echo $data[2];
+    
+    
+?>
+    
+</body>
+</html>
