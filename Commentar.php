8acd2d7e4b4ced233c1e3f2f1d110bd9e4e8e048
amandio-eto <JoseAmandiodeAlmeida021099@gmail.com>
2023-04-20 14:55:18 +0900

    update commentar

---
+++ Commentar.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Commentar</title>
+</head>
+<body>
+   
+   <h1>
+       <?php 
+        $commen = "/**/ This Use for Multiline Comentar";
+       $singlecoment = "// This is use for  Single Commentar";
+       
+       echo "$commen";
+       echo "<br>";
+       echo "$singlecoment";
+           
+           
+       
+       ?>
+   </h1>
+    
+</body>
+</html>
