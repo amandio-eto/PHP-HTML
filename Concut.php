cfba98055d04694fbe3c0430426a71b574d8104c
amandio-eto <JoseAmandiodeAlmeida021099@gmail.com>
2023-04-20 15:30:53 +0900

    update concut

---
+++ Concut.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Concut PHP</title>
+</head>
+<body>
+   <h1>
+       <?php 
+       // in PHP you can use . for Join data and etc;
+       $name = "Jose Amandio de Almeida";
+       $age = 23;
+       
+       echo $name ." ". $age;
+       
+       
+       
+       
+       ?>
+   </h1>
+    
+</body>
+</html>
--- varibale.php
+++ varibale.php
@@ -8,13 +8,18 @@
   
   <?php 
     #this is Varibale For PHP 
+    # the Simbol use for mak Varibale in PHP is $
+    # $6name -> is Variable not Invalid
+    # $name6 -> Is valid Varibale
+    # $_name = is Also Varibale Valid
+    # 
     
     $name = "Jose Amandio de Almeida";
-    $age = 23;
+    $_age = 23;
     
     echo "My Name Is :$name";
     echo "<br>";
-    echo "My Age Is :$age";
+    echo "My Age Is :$_age";
     
     
     ?>
