7b091646472b600db5a09a5f8d55382f52ce75b8
amandio-eto <JoseAmandiodeAlmeida021099@gmail.com>
2023-04-20 15:13:42 +0900

    update Line Break

---
+++ LineBreak.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Line Break in PHP</title>
+</head>
+<body>
+   
+   <h1>
+       <?php
+       
+       
+       // you want to make Brak Line in PHP  if you use HMTL you can do Line only put <br> 
+       //but you use Command line PHP syntax you can do line this PHP_EOL 
+       
+       $show = "This Is Use to Break <br>";
+       $break = "<br>";
+       $program = "My Favorite Program is PHP";
+       echo $show;
+      echo "$break";
+       echo $program;
+       
+       
+        
+       
+       ?>
+   </h1>
+    
+    
+    
+    
+    
+</body>
+</html>
